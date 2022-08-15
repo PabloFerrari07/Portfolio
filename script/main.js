b7e6d5e1c957f85c75b617bc5bcff40e386684d1
PabloFerrari <pablo.ferrari019@gmail.com>
2022-08-15 11:41:14 -0300

    cambio

--- script/main.js
+++ script/main.js
@@ -2,7 +2,12 @@ const boton = document.querySelector('#boton_dark');
 const presentacion = document.querySelector('#presentacion');
 const proyectos = document.querySelector('#proyectos');
 const conocimientos = document.querySelector('#conocimientos');
+const cuerpo = document.querySelector('body')
+
 
 boton.addEventListener('click', e =>{
     presentacion.classList.toggle('dark')
+    proyectos.classList.toggle('dark')
+    conocimientos.classList.toggle('dark')
+    body.classList.toggle('change')
 })
