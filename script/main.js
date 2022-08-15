f39abed19124d91c068ec732c06d1b6048253b6a
PabloFerrari <pablo.ferrari019@gmail.com>
2022-08-15 11:46:45 -0300

    finalizando darkmode

--- script/main.js
+++ script/main.js
@@ -2,12 +2,14 @@ const boton = document.querySelector('#boton_dark');
 const presentacion = document.querySelector('#presentacion');
 const proyectos = document.querySelector('#proyectos');
 const conocimientos = document.querySelector('#conocimientos');
-const cuerpo = document.querySelector('body')
+const cuerpo = document.querySelector('.main_all')
 
 
 boton.addEventListener('click', e =>{
     presentacion.classList.toggle('dark')
     proyectos.classList.toggle('dark')
     conocimientos.classList.toggle('dark')
-    body.classList.toggle('change')
+    cuerpo.classList.toggle('change')
+    boton.classList.toggle('white')
+    
 })
